23f119c2263ffc253f57f8d7c9b22505e1095fab
CaputoDavide93 <CaputoDavide93@Gmail.Com>
2016-03-20 18:35:20 +0100

    Login Con Controlli

---
+++ app/scripts/controllers/main.ctrl.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name orzoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the orzoApp
+ */
+angular.module('orzoApp')
+  .controller('MainCtrl', ['loggedUser' ,function (loggedUser) {
+      
+      
+    }]);
